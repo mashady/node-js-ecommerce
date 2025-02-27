4ba2777cf28d5ed7118a4c11c2d03451c83b8fb6
MUHAMMED MASHADY <muhammedmashady@gmail.com>
2025-02-27 08:46:44 +0200

    adding the project structure

---
+++ modules/auth/login/login.controller.js
@@ -0,0 +1,5 @@
+import { userModel } from "../../../database/models/User.model.js";
+import mongoose from "mongoose";
+
+const login = (req, res) => {};
+export default login;
---
+++ modules/auth/register/register.controller.js
@@ -0,0 +1,4 @@
+import { userModel } from "../../../database/models/User.model.js";
+import mongoose from "mongoose";
+const register = (req, res) => {};
+export default register;
