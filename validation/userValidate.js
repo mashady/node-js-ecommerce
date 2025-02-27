0875fa26f80aa9e0e85f1923828136ded23c4d8d
MUHAMMED MASHADY <muhammedmashady@gmail.com>
2025-02-27 18:57:05 +0200

    adding the user schema and validation

---
+++ validation/userValidate.js
@@ -0,0 +1,38 @@
+import Joi from "joi";
+
+const userSchema = Joi.object({
+  firstName: Joi.string().min(3).max(30).required().messages({
+    "string.empty": "Name cannot be empty",
+    "string.min": "Name must be at least 3 characters long",
+  }),
+  lastName: Joi.string().min(3).max(30).required().messages({
+    "string.empty": "Name cannot be empty",
+    "string.min": "Name must be at least 3 characters long",
+  }),
+  email: Joi.string().email().required().messages({
+    "string.email": "Invalid email format",
+  }),
+
+  password: Joi.string()
+    .min(6)
+    .max(20)
+    .required()
+    .pattern(new RegExp("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$"))
+    .messages({
+      "string.pattern.base":
+        "Password must contain at least one letter and one number",
+    }),
+
+  phoneNumber: Joi.string()
+    .pattern(new RegExp("^[0-9]{10,15}$"))
+    .required()
+    .messages({
+      "string.pattern.base": "Phone number must be 10-15 digits",
+    }),
+
+  role: Joi.string().valid("user", "admin", "seller").default("user"),
+
+  isVerified: Joi.boolean().default(false),
+});
+
+export default userSchema;
