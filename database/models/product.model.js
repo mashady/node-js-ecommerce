972709fba4a558cef2726b6bbd1f2e662a88e4bf
Abdelwahab <abdelwahabsaeed415@gmail.com>
2025-03-02 00:33:22 +0200

    add product schema with validation

--- database/models/product.model.js
+++ database/models/product.model.js
@@ -1,17 +1,45 @@
 import mongoose from "mongoose";
 
-const productschema = mongoose.Schema({
-  name: { type: String },
-  id: { type: Number },
-  description: { type: String },
-  category: { type: Object },
-  subcategory: { type: Object },
-  image: { type: String },
-  price: { type: Number },
-  seller: { type: Number },
-  stock: { type: Number },
-  variations: { type: Object },
-  ratings: { type: Number },
-  reviews: { type: Object },
-});
-export const productmodle = mongoose.model("product", productschema);
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 30
+    },
+    description: {
+      type: String,
+      required: true,
+      minlength: 10,
+      trim: true
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0.01, // Enforce a minimum positive price
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category', // Reference to the Category model 
+      required: true
+    },
+    images: [{
+      type: String,
+      trim: true,
+    }],
+    stock: {
+      type: Number,
+      required: true,
+      min: 0, // Enforce non-negative stock
+    },
+    reviews: [{ 
+      type: mongoose.Schema.Types.ObjectId, // Reference the Review model 
+      ref: 'Review'
+    }]
+  },
+  { timestamps: true, versionKey: false }
+);
+export const productModel = mongoose.model("Product", productSchema);
