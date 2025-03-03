4d5739b3e1253710972729542c004b0145da93e1
mashady <muhammedmashady@gmail.com>
2025-03-03 23:46:32 +0200

    product cont task

--- modules/product/product.routes.js
+++ modules/product/product.routes.js
@@ -5,21 +5,35 @@ import {
   addProduct,
   updateProduct,
   deleteProduct,
-  searchproduct
+  searchproduct,
 } from "./product.controller.js";
 
-import { validateProduct, validateProductId } from "../../middlewares/validateProduct.js";
+import {
+  validateProduct,
+  validateProductId,
+} from "../../middlewares/validateProduct.js";
 
 const productRoutes = express.Router();
 
 // Abdelwahab => Admin CRUD operations
 productRoutes.get("/products", getAllProducts);
 productRoutes.post("/addProduct", validateProduct, addProduct);
-productRoutes.put("/updateProduct/:Id", validateProductId, validateProduct, updateProduct);
+productRoutes.put(
+  "/updateProduct/:Id",
+  validateProductId,
+  validateProduct,
+  updateProduct
+);
 productRoutes.delete("/deleteProduct/:Id", validateProductId, deleteProduct);
 
-
+// heba start from here
 productRoutes.get("/products/search", searchproduct);
 productRoutes.get("/products/price", priceproduct);
 
 export default productRoutes;
+/**
+1- fetch songle product by id 
+2- search for product by name
+3- filter by price and category
+
+*/
