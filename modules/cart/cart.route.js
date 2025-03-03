2270249e950be944a2647697a17aa8a94f85c13c
mashady <muhammedmashady@gmail.com>
2025-03-03 23:17:51 +0200

    fix cart issue

--- modules/cart/cart.route.js
+++ modules/cart/cart.route.js
@@ -1,8 +1,7 @@
 import express from "express";
-import { addCart, addToCart, deleteFromCart, getCarts } from "./cart.controller.js"
-export const cartRoute = express.Router()
+import { addToCart, deleteFromCart } from "./cart.controller.js";
+export const cartRoute = express.Router();
 // testing cart schema
-cartRoute.get("/cart", getCarts)
-//  cartRoute.post("/cart", addCart)
-cartRoute.post("/cart/:productID", addToCart)
-cartRoute.delete("/cart/:productID", deleteFromCart)
+//cartRoute.get("/cart", getCarts);
+cartRoute.post("/cart/:productID", addToCart);
+cartRoute.delete("/cart/:productID", deleteFromCart);
