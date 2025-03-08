01b9001a10b947e708aadfeea34b52d8663e6791
Notebook <omarokstar2001@gmail.com>
2025-03-07 16:16:57 -0800

    Saving my changes before pulling

---
+++ validation/cartValidate.js
@@ -0,0 +1,8 @@
+import Joi from "joi";
+export const cartSchema = Joi.object({
+  quantity: Joi.number().integer().min(1).required().messages({
+    "number.base": "Quantity must be a number",
+    "number.min": "Quantity must be at least 1",
+    "any.required": "Quantity is required",
+  }),
+});
