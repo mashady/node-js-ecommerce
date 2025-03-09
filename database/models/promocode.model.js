e44ca87d1f421ebe9ec588b346fc2a061ff81d71
mashady <muhammedmashady@gmail.com>
2025-03-09 09:58:42 +0200

    0000

--- database/models/promocode.model.js
+++ database/models/promocode.model.js
@@ -1,13 +1,32 @@
 import mongoose from "mongoose";
 
 const promocodeSchema = mongoose.Schema(
-    {
-        name: { type: String, required: true, unique: true, minlength: 5, maxlength: 30 },
-
-        value: { type: Number, required: true, min: 0.00, max: 1000},
-
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 5,
+      maxlength: 30,
     },
-    { timestamps: true, versionKey: false }
+
+    value: {
+      type: Number,
+      required: true,
+      min: 0.0,
+      max: 1000,
+    },
+    addedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    updatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true, versionKey: false }
 );
 
 export const promocodeModel = mongoose.model("Promocode", promocodeSchema);
