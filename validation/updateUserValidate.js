5da3b959797ec5d1c097cd35016b05f405b533d7
mashady <muhammedmashady@gmail.com>
2025-03-21 22:24:28 +0200

    update user validation

--- validation/updateUserValidate.js
+++ validation/updateUserValidate.js
@@ -19,10 +19,33 @@ const updateProfileSchema = Joi.object({
       "string.pattern.base": "Phone number must be 10-15 digits",
     }),
 
-  address: Joi.string().optional().max(255).messages({
-    "string.max": "Address should be no longer than 255 characters",
-  }),
-
+  address: Joi.array().items(
+    Joi.object({
+      address: Joi.string().required().max(255).messages({
+        "string.max": "Address should be no longer than 255 characters",
+        "any.required": "Address is required",
+        "string.empty": "Address is empty",
+      }),
+      city: Joi.string().required().max(100).messages({
+        "string.max": "City name should be no longer than 100 characters",
+        "any.required": "City is required",
+        "string.empty": "City is empty",
+      }),
+      country: Joi.string().required().max(100).messages({
+        "string.max": "Country name should be no longer than 100 characters",
+        "any.required": "Country is required",
+        "string.empty": "Country is empty",
+      }),
+      zip: Joi.string()
+        .required()
+        .pattern(new RegExp("^[0-9]{5,10}$"))
+        .messages({
+          "string.pattern.base": "Zip code should be 5-10 digits",
+          "any.required": "Zip code is required",
+          "string.empty": "Zip code is empty",
+        }),
+    })
+  ),
   currentPassword: Joi.string().min(6).optional().messages({
     "string.min": "Current password must be at least 6 characters long",
   }),
